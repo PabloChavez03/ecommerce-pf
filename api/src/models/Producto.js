47fa4c3dd2410fedf8432a69c4eb77bc7b19763a
MatyAntunez <matias.antunez410@gmail.com>
2022-04-25 20:37:48 -0300

    configuracion inicial de los modelos

--- api/src/models/Producto.js
+++ api/src/models/Producto.js
@@ -8,5 +8,47 @@ module.exports = (sequelize) => {
 			type: DataTypes.STRING,
 			allowNull: false,
 		},
-	});
+		description: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		image: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		colors: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		size: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		listprice: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		originalprice: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		promotion_msg: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		ispromotion: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+		},
+		promotionprice: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+	},{ timestamps: false });
 };
+
+
+/*
+-----------------
+Codigo,
+*/
