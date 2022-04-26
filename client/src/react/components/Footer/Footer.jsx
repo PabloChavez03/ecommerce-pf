01896f2ffe25d18eea63b40c2e3c3d7f55636086
CasanovaRamiro <Casanovaramiroj@gmail.com>
2022-04-25 21:04:49 -0300

    Footer basics

---
+++ client/src/react/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <button>About Us!</button>
+        <button>Instagram</button>    
+        <button>Whats App!</button>    
+
+    </div>
+  )
+}
+
+export default Footer
