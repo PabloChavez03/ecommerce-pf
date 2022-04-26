be32c25af1093afb5cf5a900cbead17fbd46e8d9
CasanovaRamiro <Casanovaramiroj@gmail.com>
2022-04-26 15:26:23 -0300

    Cambie rutas en (app.js) cree CategoryLanding con styles basicos y un Landing

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,22 @@ import "./App.css";
 
 import CardDetail from "./react/components/CardDetail/CardDetail";
 import Home from "./react/components/Home/Home.jsx";
+import Landing from "./react/components/Landing/Landing";
 import NavBar from "./react/components/NavBar/NavBar";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<>
-				<Routes>
-					<Route path={"/"} element={<NavBar />} />
-					<Route exact path={"/"} element={<Home />} />
-					<Route path={"/detail/:idProduct"} element={<CardDetail />} />
-				</Routes>
-			</>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <>
+        <NavBar />
+        <Routes>
+          <Route path={"/"} element={<Landing />} />
+          <Route path={"/home"} element={<Home />} />
+          <Route path={"/detail/:idProduct"} element={<CardDetail />} />
+        </Routes>
+      </>
+    </BrowserRouter>
+  );
 }
 
 export default App;
