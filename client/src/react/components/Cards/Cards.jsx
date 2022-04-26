397ea859722172ea690d5fd123576524e009f693
micaelamedina <micadm237@gmail.com>
2022-04-25 21:02:15 -0300

    Creacion de componente Cards

--- client/src/react/components/Cards/Cards.jsx
+++ client/src/react/components/Cards/Cards.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
-export default function Cards(props) {
+export default function Cards({name, price, ofertPrice, category, image, id}) {
     return (
         <div>
-            <h1>Cards!</h1>
+            <h3>{name}</h3>
+            <img src={image} alt="Product Image"/>
+            <h5>{price}</h5>
+            {
+                ofertPrice ? <h5>{ofertPrice}</h5> : null
+            }
+            <NavLink to={`/detail/${id}`} style={{ textDecoration: 'none' }}><button>Mostrar Detalles</button></NavLink>
         </div>
     );
 };
