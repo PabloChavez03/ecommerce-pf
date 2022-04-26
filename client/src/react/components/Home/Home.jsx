7463854af531ffa24a0ad7d704ca671f29620832
CasanovaRamiro <Casanovaramiroj@gmail.com>
2022-04-25 21:22:26 -0300

    added footer to Home

--- client/src/react/components/Home/Home.jsx
+++ client/src/react/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Footer from "../Footer/Footer";
 
 export default function Home() {
     return (
         <div>
             <h1>Home!</h1>
+            <Footer/>
         </div>
     );
 };
