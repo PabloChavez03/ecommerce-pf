8f2433dbce042107f128a4505c26a03f75d47d9f
bra-i-am <brayanceron98@gmail.com>
2022-04-26 10:42:33 -0500

    Modificaciones menores de componentes para lograr el npm start en client

--- client/src/react/components/Cards/Cards.jsx
+++ client/src/react/components/Cards/Cards.jsx
@@ -2,27 +2,27 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Cards({
-  name,
-  price,
-  ofertPrice,
-  category,
-  image,
-  id,
+	name,
+	price,
+	ofertPrice,
+	category,
+	image,
+	id,
 }) {
-  return (
-    <div>
-      <h3>{name}</h3>
+	return (
+		<div>
+			<h3>{name}</h3>
 
-      <img src={image} alt="Product Image" />
-      <h4>{category}</h4>
+			<img src={image} alt="Product Img" />
+			<h4>{category}</h4>
 
-      <img src={image} alt="Product Img" />
+			<img src={image} alt="Product Img" />
 
-      <h5>{price}</h5>
-      {ofertPrice ? <h5>{ofertPrice}</h5> : null}
-      <NavLink to={`/detail/${id}`} style={{ textDecoration: "none" }}>
-        <button>Mostrar Detalles</button>
-      </NavLink>
-    </div>
-  );
+			<h5>{price}</h5>
+			{ofertPrice ? <h5>{ofertPrice}</h5> : null}
+			<NavLink to={`/detail/${id}`} style={{ textDecoration: "none" }}>
+				<button>Mostrar Detalles</button>
+			</NavLink>
+		</div>
+	);
 }
