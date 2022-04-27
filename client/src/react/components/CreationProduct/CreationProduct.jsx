d8e3a6a4399f5bd15ed367822b68cf36d5919915
micaelamedina <micadm237@gmail.com>
2022-04-27 11:58:27 -0300

    creacion rama frontend

--- client/src/react/components/CreationProduct/CreationProduct.jsx
+++ client/src/react/components/CreationProduct/CreationProduct.jsx
@@ -20,6 +20,7 @@ export default function CreationProduct() {
     });
     
     const colores = ["Blanco", "Negro", "Violeta", "Naranja"];
+
     const categorias = [
         "Women",
         "Plus + Curve",
@@ -31,6 +32,7 @@ export default function CreationProduct() {
         "Collections",
         "Sale"
     ];
+    
     const tipos = ["Remeras", "Pantalones", "Gafas", "Medias"];
 
     const handleChangeInput = (event) => {
