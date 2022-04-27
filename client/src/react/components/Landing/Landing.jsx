a8ea38439204530989c64ebc35289a482cb56085
micaelamedina <micadm237@gmail.com>
2022-04-27 12:47:20 -0300

    cambios en componente landing

--- client/src/react/components/Landing/Landing.jsx
+++ client/src/react/components/Landing/Landing.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import CategoryLanding from "../CategoryLanding/CategoryLanding";
-import { NavLink } from "react-router-dom";
 
 function Landing() {
   return (
     <div>
-      <NavLink to={"/home"}>
-        <button>Home</button>
-      </NavLink>
       <CategoryLanding />
     </div>
   );
-}
+};
 
 export default Landing;
