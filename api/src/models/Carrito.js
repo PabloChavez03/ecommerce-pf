7eaeba1cb78f9b004ed182bbcb4ecd4d811f7c0b
MatyAntunez <matias.antunez410@gmail.com>
2022-04-28 16:24:19 -0300

    Creacion modelos carrito y cliente

---
+++ api/src/models/Carrito.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require("sequelize");
+module.exports = (sequelize) => {
+    sequelize.define("carrito", { 
+        id_carrito: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey:true,
+            unique: true
+        },
+        id_product: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+    },{ timestamps: false })
+};
