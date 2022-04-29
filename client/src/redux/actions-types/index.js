8202b7371367c6aa2f9f3259273521af11d88ec5
CasanovaRamiro <Casanovaramiroj@gmail.com>
2022-04-29 17:15:21 -0300

    agregue acciones en flujo redux para poder borrar y cambiar la cantidad en carrito (a revisar)

--- client/src/redux/actions-types/index.js
+++ client/src/redux/actions-types/index.js
@@ -1,14 +1,30 @@
-import { GET_PRODUCT_BY_NAME, ADD_PRODUCT_TO_CART } from "../actions-creators";
+import {
+  GET_PRODUCT_BY_NAME,
+  ADD_PRODUCT_TO_CART,
+  REMOVE_PRODUCT_FROM_CART,
+  CHANGE_CART_QUANTITY,
+} from "../actions-creators";
 
 export const getProductByName = (nameProduct) => {
-    return async function (dispatch) {
-        //Acá iria la constante creada donde guardamos el listado de productos que coinciden con el nombre.
-        return dispatch({type: GET_PRODUCT_BY_NAME, payload: nameProduct}) //nameProduct provisoriamente hasta que tengamos creada la constante que trae los productos.
-    };   
+  return async function (dispatch) {
+    //Acá iria la constante creada donde guardamos el listado de productos que coinciden con el nombre.
+    return dispatch({ type: GET_PRODUCT_BY_NAME, payload: nameProduct }); //nameProduct provisoriamente hasta que tengamos creada la constante que trae los productos.
+  };
 };
 
 export const addProductToCart = (product) => {
-    return async function (dispatch) {
-        return dispatch({type: ADD_PRODUCT_TO_CART, payload: product})
-    };
+  return async function (dispatch) {
+    return dispatch({ type: ADD_PRODUCT_TO_CART, payload: product });
+  };
+};
+
+export const removeProductFromCart = (product) => {
+  return async function (dispatch) {
+    return dispatch({ type: REMOVE_PRODUCT_FROM_CART, payload: product });
+  };
+};
+export const changeCartQuantity = (product) => {
+  return async function (dispatch) {
+    return dispatch({ type: CHANGE_CART_QUANTITY, payload: product });
+  };
 };
--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,23 +1,41 @@
-import { ADD_PRODUCT_TO_CART, GET_PRODUCT_BY_NAME } from "../actions-creators";
+import {
+  ADD_PRODUCT_TO_CART,
+  GET_PRODUCT_BY_NAME,
+  REMOVE_PRODUCT_FROM_CART,
+  CHANGE_CART_QUANTITY,
+} from "../actions-creators";
 
 const initialState = {
-    products: [],
-    productFilter: [],
-    cartItems: [],
+  products: [],
+  productFilter: [],
+  cartItems: [],
 };
 
-export default function rootReducer (state = initialState, {type, payload}) {
-    switch(type) {
-        case GET_PRODUCT_BY_NAME:
-            return {
-                ...state,
-                productFilter: payload
-    };
-        case ADD_PRODUCT_TO_CART:
-            return {
-                ...state,
-                cartItems: [...state.cartItems, payload]
-            };
-        default: return {...state};
-    };
-};
+export default function rootReducer(state = initialState, { type, payload }) {
+  switch (type) {
+    case GET_PRODUCT_BY_NAME:
+      return {
+        ...state,
+        productFilter: payload,
+      };
+    case ADD_PRODUCT_TO_CART:
+      return {
+        ...state,
+        cartItems: [...state.cartItems, { payload, quantity: 1 }],
+      };
+    case REMOVE_PRODUCT_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((e) => e.id !== payload.id),
+      };
+    case CHANGE_CART_QUANTITY:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((e) =>
+          e.id !== payload.id ? (e.quantity = payload.quantity) : e.quantity
+        ),
+      };
+    default:
+      return { ...state };
+  }
+}
