feb7a58183f40305032c4023ead7aef5bf135c06
bra-i-am <brayanceron98@gmail.com>
2022-04-29 17:26:46 -0500

    Pequeño refact getAllProducts

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -12,5 +12,6 @@ router.use("/client", require("./client"));
 router.use("/products", require("./products"));
 // router.use("/products", require("./productsByName"));
 router.use("/products/detail", require("./productsDetail"));
+router.use("/allproducts", require("../routes/routers/getAllProducts"));
 
 module.exports = router;
