d18c55011e9ad013feefaa9a683f57731198599b
Julio Kenneth Andersson Mazuelos Vargas <kennethmazuelosvargas.km@gmail.com>
2022-04-30 12:34:01 -0500

    Ruta para filtrar de productos por genero

--- api/src/controllers/productsApiGetter.js
+++ api/src/controllers/productsApiGetter.js
@@ -1,38 +1,29 @@
+
+  
 const axios = require("axios");
 const adaptListProducts = require("./productsAdapter");
-//////////////////////////////////////////////////
-const fs = require('fs');
-const json_products = fs.readFileSync('src/data/products.json', 'utf-8');
-let products = JSON.parse(json_products);
-//////////////////////////////////////////////////
+
 async function getAPIproducts(categoryId) {
 	const options = {
 
-		method: "GET",
-		url: "https://asos2.p.rapidapi.com/products/v2/list",
-		params: {
-			store: "US",
-			offset: "0",
-			categoryId: `${categoryId}`,
-			limit: "48",
-			// q: query
-		},
-		headers: {
-			"X-RapidAPI-Host": "asos2.p.rapidapi.com",
-			"X-RapidAPI-Key": "38740551f8msh9cdf697f1473412p1dbea0jsnf76410f1a466",
-		},
-	};
+    method: "GET",
+    url: "https://asos2.p.rapidapi.com/products/v2/list",
+    params: {
+      store: "US",
+      offset: "0",
+      categoryId: `${categoryId}`,
+      limit: "48",
+      // q: query
+    },
+    headers: {
+      "X-RapidAPI-Host": "asos2.p.rapidapi.com",
+      "X-RapidAPI-Key": "324a0d5d1emshad70a2c958f1e4cp140a43jsn270daa51ceb2",
+    },
+  };
 
-	let data = await axios(options)
+	return axios(options)
 		.then(({ data }) => adaptListProducts(data))
 		.catch((e) => console.log(e.message));
-
-	products.push(data)
-	products = products.flat()
-	const json_products = JSON.stringify(products);
-	fs.writeFileSync('src/data/products.json', json_products, 'utf-8');
-
-	return products
 }
 
 module.exports = getAPIproducts;
