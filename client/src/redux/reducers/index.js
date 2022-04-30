19db77346acdf4773b6f1386ca1f280d2a3b7689
bra-i-am <brayanceron98@gmail.com>
2022-04-29 21:11:30 -0500

    fix(searchBar): Conecté la searchbar a la ruta del Backend y ya se cargan los global states con los productos filtrados

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,23 +1,27 @@
 import { ADD_PRODUCT_TO_CART, GET_PRODUCT_BY_NAME } from "../actions-creators";
 
 const initialState = {
-    products: [],
-    productFilter: [],
-    cartItems: [],
+	products: [],
+	productFilter: [],
+	cartItems: [],
 };
 
-export default function rootReducer (state = initialState, {type, payload}) {
-    switch(type) {
-        case GET_PRODUCT_BY_NAME:
-            return {
-                ...state,
-                productFilter: payload
-    };
-        case ADD_PRODUCT_TO_CART:
-            return {
-                ...state,
-                cartItems: [...state.cartItems, payload]
-            };
-        default: return {...state};
-    };
-};
+export default function rootReducer(state = initialState, { type, payload }) {
+	switch (type) {
+		case GET_PRODUCT_BY_NAME:
+			console.log(payload);
+			return {
+				...state,
+				products: payload,
+				productFilter: payload,
+			};
+
+		case ADD_PRODUCT_TO_CART:
+			return {
+				...state,
+				cartItems: [...state.cartItems, payload],
+			};
+		default:
+			return { ...state };
+	}
+}
