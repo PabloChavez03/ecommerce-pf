aebf2c04787f9572af14f515a14c57fe4a52315d
CasanovaRamiro <Casanovaramiroj@gmail.com>
2022-04-30 22:47:24 -0300

    cards styles

--- client/src/react/components/Cards/Cards.jsx
+++ client/src/react/components/Cards/Cards.jsx
@@ -17,7 +17,7 @@ export default function Cards({
     <div className={css.container}>
       <img src={image} alt="Product Img" />
       <div className={css.price}>
-        <h3>{isOffertPrice ? previousPrice : currentPrice}</h3>
+        <h3>{isOffertPrice ? previousPrice : `$ ${currentPrice} `}</h3>
         {isOffertPrice ? <h4>{`Precio de oferta $ ${currentPrice}`}</h4> : null}
       </div>
       <h5 className={css.title}>{name}</h5>
