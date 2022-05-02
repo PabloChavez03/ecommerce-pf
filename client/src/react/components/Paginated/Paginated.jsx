13123495058371566296c24976d6fef46f710b41
micaelamedina <micadm237@gmail.com>
2022-05-02 02:18:17 -0300

    Estilos en paginado

--- client/src/react/components/Paginated/Paginated.jsx
+++ client/src/react/components/Paginated/Paginated.jsx
@@ -2,14 +2,17 @@ import React, { useState } from "react";
 import {useSelector, useDispatch} from 'react-redux';
 import { setCurrentPage } from "../../../redux/actions-types";
 import style from "./Paginated.module.css";
+import prev from "./images/left.png";
+import next from "./images/arrow-right.png";
 
 export default function Paginated({allProducts, productsPerPage}) {
     const dispatch = useDispatch();
     const currentPage = useSelector((state) => state.currentPage);
-    const [limitNumberPage] = useState(5); //limite de botones con numero de pag que quiero mostrar.
-    const [maxLimitNumberPage, setmaxLimitNumberPage] = useState(5); //numero limite maximo de pagina.(la ultima) por ej si renderiza 5, la primera vez sera 5, la segunda vez sera 10 y asi sucesivamente.
-    const [minLimitNumberPage, setminLimitNumberPage] = useState(0); //numero minimo de pagina. (la primera)
+    const [limitNumberPage] = useState(5);
+    const [maxLimitNumberPage, setmaxLimitNumberPage] = useState(5);
+    const [minLimitNumberPage, setminLimitNumberPage] = useState(0);
     const numberPage = [];
+
     for (let i = 1; i <= Math.ceil(allProducts.length / productsPerPage); i++) {
             numberPage.push(i);
     };
@@ -18,32 +21,35 @@ export default function Paginated({allProducts, productsPerPage}) {
         dispatch(setCurrentPage(parseInt(event.target.id)));
     };
 
-    const handleNextPage = () => {
-        dispatch(setCurrentPage(parseInt(currentPage + 1)))
-        
-        if(currentPage + 1 > maxLimitNumberPage) {
-            setmaxLimitNumberPage(maxLimitNumberPage + limitNumberPage);
-            setminLimitNumberPage(minLimitNumberPage + limitNumberPage);
+    const handleNextPage = (event) => {
+        event.preventDefault();
+        if((currentPage+1) !== Math.ceil(allProducts.length / productsPerPage)) {
+            dispatch(setCurrentPage(parseInt(currentPage + 1)))
+            if(currentPage + 1 > maxLimitNumberPage) {
+                setmaxLimitNumberPage(maxLimitNumberPage + limitNumberPage);
+                setminLimitNumberPage(minLimitNumberPage + limitNumberPage);
+            };   
         };
     };
 
-      const handlePrevPage = () => {
-          dispatch(setCurrentPage(parseInt(currentPage - 1)))
-          if(parseInt((currentPage - 1) % limitNumberPage) === 0) {
-              setmaxLimitNumberPage(maxLimitNumberPage - limitNumberPage);
-              setminLimitNumberPage(minLimitNumberPage - limitNumberPage);
-          };
+    const handlePrevPage = (event) => {
+        event.preventDefault();
+        if((currentPage-1) !== 0 ) {
+            dispatch(setCurrentPage(parseInt(currentPage - 1)))
+            if(parseInt((currentPage - 1) % limitNumberPage) === 0) {
+                setmaxLimitNumberPage(maxLimitNumberPage - limitNumberPage);
+                setminLimitNumberPage(minLimitNumberPage - limitNumberPage);
+            };    
+        };              
       };
-
 
     return (
         <div>
             {
                 allProducts.length && <nav>
                 <ul className={style.numberPage}>
-                    <button onClick={handlePrevPage} disabled={parseInt(currentPage) === 1 ? true : false}>{"<<"} </button>
-
-                    {/* {pageDecrement} */}
+                    <img className={style.imgPrev} src={prev} alt="Previous" onClick={(e)=>handlePrevPage(e)}/>
+                    {/* <button onClick={handlePrevPage} disabled={parseInt(currentPage) === 1 ? true : false}>{"<<"} </button> */}
                     {
                             numberPage.length >= 1?numberPage.map((n,i) => {
                                 
@@ -57,14 +63,13 @@ export default function Paginated({allProducts, productsPerPage}) {
                             }})
                             :<p>Loading...</p>
                     }
-                    {/* {pageIncrement} */}
-                    <button 
+                    <img className={style.imgNext} src={next} alt="Previous" onClick={(e)=>handleNextPage(e)}/>
+                    {/* <button 
                     onClick={handleNextPage}
-                    disabled={parseInt(currentPage) === Math.ceil(allProducts.length / productsPerPage) ? true : false}>{">>"}</button>
-
+                    disabled={parseInt(currentPage) === Math.ceil(allProducts.length / productsPerPage) ? true : false}>{">>"}</button> */}
                 </ul>
             </nav>
             }
         </div>
-    )
-}
+    );
+};
--- client/src/react/components/SearchBar/SearchBar.jsx
+++ client/src/react/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { getProductByName } from "../../../redux/actions-types";
+import lupa from "./images/lupa-de-busqueda.png";
+import style from "./SearchBar.module.css";
 
 export default function SearchBar() {
 	const dispatch = useDispatch();
@@ -31,6 +33,7 @@ export default function SearchBar() {
 					onChange={(e) => handleChangeInput(e)}
 				/>
 				<button onClick={(e) => handleClickSearch(e)}>Buscar</button>
+				{/* <img className={style.imgSearch} src={lupa} alt="Search" onClick={(e)=>handleClickSearch(e)}/> */}
 			</form>
 		</div>
 	);
