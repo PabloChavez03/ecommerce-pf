4a98dae2259bc2e86ec392c9dfd36334a7bbeb37
davidMachuca <davidmachucarodriguez@gmail.com>
2022-05-03 10:43:30 -0500

    Creando componente admin

--- client/src/App.js
+++ client/src/App.js
@@ -8,21 +8,23 @@ import Landing from "./react/components/Landing/Landing";
 import NavBar from "./react/components/NavBar/NavBar";
 import ProductCreate from "./react/components/CreationProduct/ProductCreate";
 import Footer from "./react/components/Footer/Footer";
+import Admin from "./react/AdminComponents/Pages/Admin";
 
 function App() {
 	return (
 		<BrowserRouter>
-				<NavBar />
-				<Routes>
-					<Route path={"/"} element={<Landing />} />
-					<Route path={"/home"} element={<Home />} />
-					<Route path={"/detail/:productId"} element={<CardDetail />} />
-					<Route path={"/creation"} element={<ProductCreate />} />
-					<Route path={"/about"} element={<Construction />} />
-					<Route path={"/account"} element={<Construction />} />
-					<Route path={"/construction"} element={<Construction />} />
-				</Routes>
-				<Footer/>
+			<NavBar />
+			<Routes>
+				<Route path={"/"} element={<Landing />} />
+				<Route path={"/home"} element={<Home />} />
+				<Route path={"/detail/:productId"} element={<CardDetail />} />
+				<Route path={"/creation"} element={<ProductCreate />} />
+				<Route path={"/about"} element={<Construction />} />
+				<Route path={"/account"} element={<Construction />} />
+				<Route path={"/construction"} element={<Construction />} />
+				<Route path={"/admin"} element={<Admin />} />
+			</Routes>
+			<Footer />
 		</BrowserRouter>
 	);
 }
