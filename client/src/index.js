a03fdc12f9aa8a2887bbff51c267ed0d50acdb2d
micaelamedina <micadm237@gmail.com>
2022-05-04 15:23:25 -0300

    cambios

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,6 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store/index";
 import axios from "axios";
-import dotenv from 'dotenv';
-dotenv.config();
 axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
