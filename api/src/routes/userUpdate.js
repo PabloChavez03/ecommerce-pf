830ded7465af43ada71033ea9dc56d5adec5be98
Pablo Chavez <pablizinho187@gmail.com>
2022-05-04 00:01:13 -0300

    CRUD USER complete

--- api/src/routes/userUpdate.js
+++ api/src/routes/userUpdate.js
@@ -7,8 +7,11 @@ router.patch("/:legajo_user", async (req, res) => {
   const { user_name, user_password, rol } = req.body;
   let { legajo_user } = req.params;
 
+  const saltRounds = 10;
+  let newPassword = await bcrypt.hash(user_password, saltRounds);
+
   try {
-   const modified = Users.update({ legajo_user, user_name, user_password, rol}, {
+   const modified = Users.update({ legajo_user, user_name, user_password: newPassword, rol}, {
         where: {legajo_user}
     });
     res.status(200).send(`${modified} Usuario modificado`)
