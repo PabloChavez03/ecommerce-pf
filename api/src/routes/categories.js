20a706377450587b6da5debcffbf14744d8df807
bra-i-am <brayanceron98@gmail.com>
2022-05-04 17:42:18 -0500

    feat(back): Ruta de creación de categorías

--- api/src/routes/categories.js
+++ api/src/routes/categories.js
@@ -1,5 +1,5 @@
 const getDDBBcategories = require("../controllers/categoriesDbGetter");
-
+const { Category } = require("../db");
 
 const { Router } = require("express");
 const router = Router();
@@ -8,6 +8,22 @@ router.get("", async (_req, res) => {
 	const categoriesFound = await getDDBBcategories();
 
 	res.json(categoriesFound);
+});
+
+router.post("", async (req, res) => {
+	const { id, title, genre } = req.body;
+
+	let [categoryCreated, created] = await Category.findOrCreate({
+		where: {
+			id,
+			title,
+			genre,
+		},
+	}).catch((e) => console.log(e));
+
+	created
+		? res.status(201).send("Creado exitosamente")
+		: res.send("Producto existente");
 });
 
 module.exports = router;
