10bf1c58a521d16ab23ee01480d40c6d40b2112d
MatyAntunez <matias.antunez410@gmail.com>
2022-05-07 16:44:32 -0300

    ruta get find user by pk

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -29,7 +29,8 @@ router.use("/products/delete", [authMaster, isAdmin],	require("./deleteProduct")
 router.use("/product/stock", [authMaster, isAdmin], require("./updateStock"));
 router.use("/users/update", [authMaster, isAdmin], require("./userUpdate"));
 router.use("/users/delete", [authMaster, isAdmin], require("./userDelete"));
-router.use("/users/findall", [authMaster, isAdmin], require("./getUsers"));
+router.use("/users/findall", require("./getUsers"));
+router.use("/users/findByPk", require("./usersFindByPk"));
 //----------------------Mercado Pago---------------------------------------
 router.use("/mercadopago", require("./mercadoPago"));
 //-------------------------------------------------------------------------
