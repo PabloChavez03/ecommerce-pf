eb0162df0660721fe396996417be1f4b07c7c83e
bra-i-am <brayanceron98@gmail.com>
2022-05-09 11:05:38 -0500

    fix(shoppingBag): Se había roto

--- client/src/react/components/ShoppingCart/ShoppingBag/ShoppingBag.jsx
+++ client/src/react/components/ShoppingCart/ShoppingBag/ShoppingBag.jsx
@@ -1,23 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import NavBar from "../../NavBar/NavBar";
 import ProductCardModal from "../CardModal/ProductCardModal";
-import css from "./ShoppingBag.module.css";
+
+import axios from "axios";
 
 export default function ShoppingBag() {
-	const navigate = useNavigate();
 	const cartItems = useSelector((state) => state.cartItems);
-	const userData = useSelector((state) => state.userData);
-
 	let suma = 0;
 	let subtotal = cartItems?.forEach((e) => (suma += e.currentPrice));
 	let envio = 50;
 
-    const handleClickSend = (e) => {
-        e.preventDefault();
-        alert("Funcionalidad en desarrollo!");
-    };
+	const [email, setEmail] = useState("");
+	const [linkMP, setLinkMP] = useState("");
 
 	const handleClickSend = (e) => {
 		e.preventDefault();
@@ -26,85 +22,56 @@ export default function ShoppingBag() {
 
 	const handlePayment = async (e) => {
 		e.preventDefault();
-		if(suma === 0){alert('Debe agregar productos al carrito para continuar'); navigate('/'); return}
-		if(userData.rol === 'admin'){alert('Un administrador no puede realizar compras'); return}
-		const emailAux = userData.email ? userData.email : email;
-
 		const { data } = await axios.get(
 			"http://localhost:3001/mercadopago/payment",
 			{
-				params: { cartItems, emailAux, envio },
+				params: { cartItems, email, envio },
 			},
 		);
 
-		window.open(data.init_point, "_self");
+		console.log(data.init_point);
 	};
 
 	return (
-		<div className={css.container}>
-			<div className={css.nav}>
-				<NavBar />
-			</div>
-			<h1 className={css.name}>Mi Carrito</h1>
-			<div className={css.modalCartContainer}>
-				{cartItems.length ? (
-					cartItems.map((e) => (
-						<ProductCardModal
-							id={e.id}
-							name={e.name}
-							color={e.color}
-							price={e.currentPrice}
-							image={e.image}
-							size={e.brandSize}
-							quantity={e.quantity}
-							key={e.id + e.brandSize}
-						/>
-					))
-				) : (
-					<p>No hay productos en el carrito</p>
-				)}
-			</div>
-			<div className={css.infoCont}>
-				<h3>Subtotal:</h3>
-				<p>${suma}</p>
-				<h3>Costo de envío:</h3>
-				<p>${envio}</p>
-				<h2>Total:</h2>
-				<p>${suma + envio}</p>
+		<div>
+			<NavBar />
+			<h1>Mi Carrito</h1>
+			{cartItems.length ? (
+				cartItems.map((e) => (
+					<ProductCardModal
+						id={e.id}
+						name={e.name}
+						color={e.color}
+						price={e.currentPrice}
+						image={e.image}
+						quantity={e.quantity}
+						key={e.id}
+					/>
+				))
+			) : (
+				<p>No hay productos en el carrito</p>
+			)}
+			<h3>Subtotal:</h3>
+			<p>${suma}</p>
+			<h3>Costo de envío:</h3>
+			<p>${envio}</p>
+			<h2>Total:</h2>
+			<p>${suma + envio}</p>
 
-				{(userData.email && !userData.email.length) || !userData.email ? (
-					<div className={css.mail}>
-						<label htmlFor="email">Email: </label>
-						<br />
-						<i>Recuerda agregar tu email en tu información de perfil</i>
-						<br />
-						<input
-							id="email"
-							type="email"
-							onChange={(e) => setEmail(e.target.value)}
-						/>
-					</div>
-				) : (
-					""
-				)}
-				<br />
-				{userData.username ? (
-					<>
-						<NavLink to={"/pay"}>
-							<button className={css.btn} onClick={(e) => handlePayment(e)}>
-								Ir a pagar
-							</button>
-						</NavLink>
-					</>
-				) : (
-					"Inicia sesión para comprar"
-				)}
+			<label htmlFor="email">Email: </label>
+			<input
+				id="email"
+				type="email"
+				onChange={(e) => setEmail(e.target.value)}
+			/>
 
-				<br></br>
-				<NavLink to={"/"}>
-					<button className={css.btn}>Seguir comprando</button>
-				</NavLink>
-			</div>
+			<NavLink to={"/pay"}>
+				<button onClick={(e) => handlePayment(e)}>Finalizar compra</button>
+			</NavLink>
+			<br></br>
+			<NavLink to={"/"}>
+				<button>Seguir comprando</button>
+			</NavLink>
 		</div>
 	);
 }
