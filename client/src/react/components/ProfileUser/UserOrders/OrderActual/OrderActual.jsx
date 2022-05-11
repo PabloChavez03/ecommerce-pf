fc0e63445b8f82d8681396db2e8a365b168ea0c1
CasanovaRamiro <Casanovaramiroj@gmail.com>
2022-05-11 16:48:38 -0300

    Order Actual componente para ordenes recien creadas

---
+++ client/src/react/components/ProfileUser/UserOrders/OrderActual/OrderActual.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import css from './OrderActual.module.css'
+function OrderActual() {
+  return (
+    <div className={css.container}>
+        <h1> ORDENESSSSS</h1>
+    </div>
+  )
+}
+
+export default OrderActual
