46125e43477688d3f956522475057e604aa598e5
CasanovaRamiro <Casanovaramiroj@gmail.com>
2022-05-11 17:16:21 -0300

    compoinente ordenes merge con frontaux

--- client/src/react/components/ProfileUser/UserOrders/OrderActual/OrderActual.jsx
+++ client/src/react/components/ProfileUser/UserOrders/OrderActual/OrderActual.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
 import css from './OrderActual.module.css'
+import {  useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+
 function OrderActual() {
+    const cartItems = useSelector((state) => state.cartItems);
+  const {email} = useSelector((state) => state.userData);
+  const subTotal = useSelector((state) => state.subTotal);
+  const dispatch = useDispatch()
+    const location = useLocation();
+    console.log(email)
+
+	const params = location.search
+		.slice(1)
+		.split("&")
+		.map((el) => el.split("="));
+
+	const {
+		collection_id,
+		collection_status,
+		payment_id,
+		status,
+		external_reference,
+		payment_type,
+		merchant_order_id,
+		preference_id,
+		site_id,
+		processing_mode,
+		merchant_account_id,
+	} = Object.fromEntries(params);
+    
+    
   return (
     <div className={css.container}>
         <h1> ORDENESSSSS</h1>
+        <h1>collection_status: {collection_status}</h1>
+        <h1>payment_id: {payment_id}</h1>
+        <h1>status: {status}</h1>
+        <h1>external_reference: {external_reference}</h1>
+        <h1>payment_type: {payment_type}</h1>
+        <h1> merchant_order_id: {merchant_order_id}</h1>
+        <h1>preference_id: {preference_id}</h1>
+        <h1>site_id: site_id</h1>
+        <h1>processing_mode: {processing_mode}</h1>
+        <h1> merchant_account_id: {merchant_account_id}</h1>
+        
     </div>
   )
 }
