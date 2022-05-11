a69257cb311aa921e1c2ef71466ecd9f84be9400
Julio Kenneth Andersson Mazuelos Vargas <kennethmazuelosvargas.km@gmail.com>
2022-05-10 19:21:11 -0500

    Actializar mail

--- api/src/controllers/mailer.js
+++ api/src/controllers/mailer.js
@@ -6,7 +6,7 @@ let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
     secure: true, // true for 465, false for other ports
-    service:'gmail',
+    service: 'gmail',
     auth: {
         user: USER_GOOGLE, // generated ethereal user
         pass: PASS_GOOGLE, // generated ethereal password
@@ -15,6 +15,27 @@ let transporter = nodemailer.createTransport({
 transporter.verify().then(() => {
     console.log('Ready for send emails');
 })
+
+
+async function publicidadEmail({ emailGoogle, emailAsunto, emailImagenTitle, emailTitulo, emailSubTitle, emailDescription }) {
+    await transporter.sendMail({
+        from: `"PRUEBA XD👻" ${USER_GOOGLE}`,
+        to: emailGoogle,
+        subject: emailAsunto,
+        html: `
+        <div style="width:60%;margin:auto;">
+            ${emailImagenTitle ?
+                `<img style="width:100%" src=${emailImagenTitle} />`
+                : ''}
+            <h1 style="color:blue">${emailTitulo}</h1>
+            <h2 style="color:black">${emailSubTitle}</h2>
+            <p style="color:black">${emailDescription}</p>
+        </div>
+        `
+    })
+}
+
 module.exports = {
-    transporter
+    transporter,
+    publicidadEmail
 }
--- api/src/routes/mailer.js
+++ api/src/routes/mailer.js
@@ -1,5 +1,6 @@
+const { text } = require("body-parser");
 const { Router } = require("express");
-const { transporter } = require("../controllers/mailer");
+const { transporter, publicidadEmail } = require("../controllers/mailer");
 require("dotenv").config();
 const { USER_GOOGLE, PASS_GOOGLE } = process.env;
 const router = Router();
@@ -9,19 +10,31 @@ router.get('/', async (req, res) => {
     try {
         // send mail with defined transport object
         await transporter.sendMail({
-            from: `"PRUEBA XD👻" ${USER_GOOGLE}`, // sender address
-            to: emailGoogle, // list of receivers
-            subject: "Hello ✔", // Subject line
+            from: `"PRUEBA XD👻" ${USER_GOOGLE}`,
+            to: emailGoogle,
+            subject: "Hello ✔",
             html: `
             <b>Plase click in the following link, or paste this into your browser to complete the process:</b>
             <p>XD</p>
             `
         }).then(() => res.json("logrado"));
-
     } catch (error) {
         console.log(error)
     }
+})
 
+router.get('/publicidad', async (req, res) => {
+    const { emailGoogle, emailImagenTitle, emailAsunto, emailTitulo, emailSubTitle, emailDescription } = req.body;
+    try {
+        await publicidadEmail({ emailGoogle, emailImagenTitle, emailAsunto, emailTitulo, emailSubTitle, emailDescription })
+            .then(() => res.json({ "Info": "Enviado Publicidad" }));
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+router.get('/cliente/enviar-verificacion', async (req, res) => {
+    const { emailGoogle, emailDescription } = req.query;
 })
 
 module.exports = router;
