107344c2aa5c5aa82c8b01ec67b54ee8a3abedde
MatyAntunez <matias.antunez410@gmail.com>
2022-05-11 17:17:08 -0300

    rutas de get para ordenes

--- api/src/routes/getOrderByPk.js
+++ api/src/routes/getOrderByPk.js
@@ -4,12 +4,12 @@ const { route } = require("./postPurchaseOrder");
 const router = Router();
 
 
-router.get("/:orderId", async (req, res) => {
+router.get("/:payment_id", async (req, res) => {
 
-    const {orderId} = req.params;
+    const {payment_id} = req.params;
 
     try {
-        let findOrder = await Order.findByPk(orderId)
+        let findOrder = await Order.findByPk(payment_id)
         res.status(200).json(findOrder)
     } catch (error) {
         console.log(error)
