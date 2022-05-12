99bfc6576f20f33ebb82dbe240c9dc9f19dca3e2
bra-i-am <brayanceron98@gmail.com>
2022-05-12 11:48:12 -0500

    Implementando Wishlist en el front

--- client/src/react/components/Cards/Cards.jsx
+++ client/src/react/components/Cards/Cards.jsx
@@ -1,33 +1,36 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+import WishlistIcon from "../svg/WishlistIcon";
+
 // import Construction from "../Construction/Construction";
 import css from "./Cards.module.css";
 
 export default function Cards({
-  id,
-  name,
-  image,
-  isOffertPrice,
-  previousPrice,
-  currentPrice,
-  variants,
-  color
+	id,
+	name,
+	image,
+	isOffertPrice,
+	previousPrice,
+	currentPrice,
+	variants,
+	color,
 }) {
-//   const handleClickAddCart = (event) => {
-//     event.preventDefault(); //provisorio hasta que este el carrito
-//     alert("Funcionalidad en desarrollo");
-// }
-  return (
-    <div className={css.container}>
-        <img src={`https://${image}`} alt="Product Img" />
-      <div className={css.price}>
-        <h3>{isOffertPrice ? previousPrice : `$ ${currentPrice} `}</h3>
-        {isOffertPrice ? <h4>{`Precio de oferta $ ${currentPrice}`}</h4> : null}
-      </div>
-      <h5 className={css.title}>{name}</h5>
-      {/* <h5 className={css.title}>Color: {color}</h5> */}
-      {/* <select>
+	//   const handleClickAddCart = (event) => {
+	//     event.preventDefault(); //provisorio hasta que este el carrito
+	//     alert("Funcionalidad en desarrollo");
+	// }
+	return (
+		<div className={css.container}>
+			<WishlistIcon />
+			<img src={`https://${image}`} alt="Product Img" />
+			<div className={css.price}>
+				<h3>{isOffertPrice ? previousPrice : `$ ${currentPrice} `}</h3>
+				{isOffertPrice ? <h4>{`Precio de oferta $ ${currentPrice}`}</h4> : null}
+			</div>
+			<h5 className={css.title}>{name}</h5>
+			{/* <h5 className={css.title}>Color: {color}</h5> */}
+			{/* <select>
         <option>Talle</option>
       {
         variants?.length?variants.map((e)=>(
@@ -36,6 +39,6 @@ export default function Cards({
       }
       </select>
       <button onClick={(e)=>handleClickAddCart(e)}>Agregar al carrito</button> */}
-    </div>
-  );
+		</div>
+	);
 }
---
+++ client/src/react/components/svg/WishlistIcon.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import s from "./WishlistIcon.module.css";
+
+function WishlistIcon() {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			className={s.svg}
+			viewBox="0 0 24 24"
+		>
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+			/>
+		</svg>
+	);
+}
+
+export default WishlistIcon;
