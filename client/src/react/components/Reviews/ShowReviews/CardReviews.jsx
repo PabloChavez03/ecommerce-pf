3bf1611d03ba461d9048925c5d5a4651715b6a60
davidMachuca <davidmachucarodriguez@gmail.com>
2022-05-13 11:35:20 -0500

    agregando modal a la alerta de delete de los reviews

--- client/src/react/components/Reviews/ShowReviews/CardReviews.jsx
+++ client/src/react/components/Reviews/ShowReviews/CardReviews.jsx
@@ -9,66 +9,73 @@ import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function CardReviews({
-  UserUserName,
-  comment,
-  calification,
-  id,
+	UserUserName,
+	comment,
+	calification,
+	id,
+	modalStatus,
+	setModalStatus,
 }) {
-  const userData = useSelector((state) => state.userData).username;
-  return (
-    <div className={style.container}>
-      <AccountIcon />
-      <h4>Usuario:</h4>
-      <p>{UserUserName}</p>
-      <h4>Comentario:</h4>
-      <span>{comment}</span>
-      <h4>Calificación:</h4>
-      <div className={style.imgContainer}>
-        <img
-          src={calification >= 1 ? starGold : starBlack}
-          alt="star"
-          className={style.imgStar}
-          name={1}
-        />
-        <img
-          src={calification >= 2 ? starGold : starBlack}
-          alt="star"
-          className={style.imgStar}
-          name={2}
-        />
-        <img
-          src={calification >= 3 ? starGold : starBlack}
-          alt="star"
-          className={style.imgStar}
-          name={3}
-        />
-        <img
-          src={calification >= 4 ? starGold : starBlack}
-          alt="star"
-          className={style.imgStar}
-          name={4}
-        />
-        <img
-          src={calification >= 5 ? starGold : starBlack}
-          alt="star"
-          className={style.imgStar}
-          name={5}
-        />
-        {UserUserName === userData ? (
-          <div>
-            <NavLink to={`/user/reviews/update/${id}`}>
-              <img src={editIcon} alt="Icon Edit" className={style.imgStar} />
-            </NavLink>
-            <NavLink to={`/user/reviews/delete/${id}`}>
-              <img
-                src={deleteIcon}
-                alt="Icon Delete"
-                className={style.imgStar}
-              />
-            </NavLink>
-          </div>
-        ) : null}
-      </div>
-    </div>
-  );
+	const userData = useSelector((state) => state.userData).username;
+
+	const handleModalStatus = () => {
+		setModalStatus(!modalStatus);
+	};
+
+	return (
+		<div className={style.container}>
+			<AccountIcon />
+			<h4>Usuario:</h4>
+			<p>{UserUserName}</p>
+			<h4>Comentario:</h4>
+			<span>{comment}</span>
+			<h4>Calificación:</h4>
+			<div className={style.imgContainer}>
+				<img
+					src={calification >= 1 ? starGold : starBlack}
+					alt='star'
+					className={style.imgStar}
+					name={1}
+				/>
+				<img
+					src={calification >= 2 ? starGold : starBlack}
+					alt='star'
+					className={style.imgStar}
+					name={2}
+				/>
+				<img
+					src={calification >= 3 ? starGold : starBlack}
+					alt='star'
+					className={style.imgStar}
+					name={3}
+				/>
+				<img
+					src={calification >= 4 ? starGold : starBlack}
+					alt='star'
+					className={style.imgStar}
+					name={4}
+				/>
+				<img
+					src={calification >= 5 ? starGold : starBlack}
+					alt='star'
+					className={style.imgStar}
+					name={5}
+				/>
+				{UserUserName === userData ? (
+					<div>
+						<NavLink to={`/user/reviews/update/${id}`}>
+							<img src={editIcon} alt='Icon Edit' className={style.imgStar} />
+						</NavLink>
+
+						<img
+							src={deleteIcon}
+							alt='Icon Delete'
+							className={style.imgStar}
+							onClick={handleModalStatus}
+						/>
+					</div>
+				) : null}
+			</div>
+		</div>
+	);
 }
--- client/src/react/components/Reviews/ShowReviews/ShowReviews.jsx
+++ client/src/react/components/Reviews/ShowReviews/ShowReviews.jsx
@@ -1,37 +1,54 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
 import { getDetails } from "../../../../redux/actions-types";
 import CardReviews from "./CardReviews";
+import ShowReviewsModalDelete from "./ShowReviewsModal/ShowReviewsModalDelete";
 
 export default function ShowReviews({ productId }) {
-  const dispatch = useDispatch();
-  const {pathname} = useLocation();
-  console.log(pathname)
-  useEffect(() => {
-    dispatch(getDetails(productId));
-  }, [productId, dispatch]);
+	const dispatch = useDispatch();
+	const { pathname } = useLocation();
+	console.log(pathname);
+	useEffect(() => {
+		dispatch(getDetails(productId));
+	}, [productId, dispatch]);
 
-  const productRender = useSelector((state) => state.details);
-  const reviews = productRender.Reviews;
+	const productRender = useSelector((state) => state.details);
+	const reviews = productRender.Reviews;
 
-  return (
-    <div>
-      <h3>Reseñas del producto:</h3>
-      {reviews?.length ? (
-        <div>
-          {productRender.Reviews.map((e)=>(
-            <CardReviews
-              UserUserName={e.UserUserName}
-              comment={e.comment}
-              calification={e.calification}
-              pathname={pathname}
-            />
-          ))}
-        </div>
-      ) : (
-        <h4>Aún no hay reseñas para este producto</h4>
-      )}
-    </div>
-  );
+	/** Funcionamiento Modal */
+
+	const [modalStatus, setModalStatus] = useState(false);
+
+	/** Fin funcionamiento Modal */
+
+	return (
+		<div>
+			<h3>Reseñas del producto:</h3>
+			{reviews?.length ? (
+				<div>
+					{productRender.Reviews.map((e) => (
+						<CardReviews
+							key={e.UserUserName + e.comment}
+							UserUserName={e.UserUserName}
+							comment={e.comment}
+							calification={e.calification}
+							pathname={pathname}
+							modalStatus={modalStatus}
+							setModalStatus={setModalStatus}
+						/>
+					))}
+				</div>
+			) : (
+				<h4>Aún no hay reseñas para este producto</h4>
+			)}
+
+			{modalStatus && (
+				<ShowReviewsModalDelete
+					modalStatus={modalStatus}
+					setModalStatus={setModalStatus}
+				/>
+			)}
+		</div>
+	);
 }
