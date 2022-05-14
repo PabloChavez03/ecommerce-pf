1f1e8a28c8d4601bd44b0a4b09decae37d9cd25b
MatyAntunez <matias.antunez410@gmail.com>
2022-05-14 20:43:42 -0300

    sweetalert

--- client/src/react/components/Login/Login.jsx
+++ client/src/react/components/Login/Login.jsx
@@ -160,6 +160,23 @@ const Login = () => {
 			user_password: "",
 		});
 		dispatch(UserLogin(login));
+
+		const Toast = Swal.mixin({
+			toast: true,
+			position: 'top-end',
+			showConfirmButton: false,
+			timer: 3000,
+			timerProgressBar: true,
+			didOpen: (toast) => {
+			  toast.addEventListener('mouseenter', Swal.stopTimer)
+			  toast.addEventListener('mouseleave', Swal.resumeTimer)
+			}
+		  })
+		  
+		  Toast.fire({
+			icon: 'success',
+			title: 'Iniciado sesion con éxito!'
+		  })
 	};
 
 	const GOOGLE = () => {
