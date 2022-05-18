57455a57da06bf4b18eed9c51af839151baa0696
Pablo Chavez <pablizinho187@gmail.com>
2022-05-17 21:46:12 -0300

    finish changes and finish google

--- api/notUsed/passportJwt.js
+++ api/notUsed/passportJwt.js
@@ -2,7 +2,7 @@ const passport = require("passport");
 const passportJwt = require("passport-jwt");
 const ExtractJwt = passportJwt.ExtractJwt;
 const StrategyJwt = passportJwt.Strategy;
-const { Users,Role } = require("../db");
+const { Users,Role } = require("../src/db");
 
 passport.use(
   new StrategyJwt({
